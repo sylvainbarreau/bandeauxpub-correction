6aec7b1f733e26664c84487bd5f15dc8e48edf90
Rémi Bastide <Remi.Bastide@irit.fr>
2023-01-16 17:19:38 +0100

    A compléter

--- src/main/java/bandeau/BandeauVerrouillable.java
+++ src/main/java/bandeau/BandeauVerrouillable.java
@@ -1,2 +1,14 @@
-package bandeau;public class BandeauVerrouillable {
+package bandeau;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BandeauVerrouillable extends Bandeau {
+    private final Lock verrou = new ReentrantLock();
+    public void verrouille() {
+        verrou.lock();
+    }
+    public void déverrouille() {
+        verrou.unlock();
+    }
 }
